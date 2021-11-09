8852b5d7a4598b4ad36211fd2a7e4c40cafe4ea4
= <=>
2021-11-09 20:45:59 -0300

    challenge finished

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,50 @@
-export function Content() {
+import { MovieCard } from "./MovieCard";
+
+import "../styles/content.scss";
+import { Header } from "./Header";
+
+interface GenreResponseProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  selectedGenre: GenreResponseProps;
+  movies: Array<MovieProps>;
+}
+
+export function Content(props: ContentProps) {
   // Complete aqui
+
+  return (
+    <div className="container">
+      <Header selectedGenre={props.selectedGenre} />
+
+      <main>
+        <div className="movies-list">
+          {props.movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
